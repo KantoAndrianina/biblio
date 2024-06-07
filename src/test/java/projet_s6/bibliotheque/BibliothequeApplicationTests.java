b3bcebd3ca7ccb167223f365902643d530d218a6
KantoAndrianina <kanto.andrianina@yahoo.com>
2024-06-07 11:53:48 +0300

    init

---
+++ src/test/java/projet_s6/bibliotheque/BibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package projet_s6.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
