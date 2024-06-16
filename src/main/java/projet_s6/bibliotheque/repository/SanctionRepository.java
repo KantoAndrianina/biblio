ac4dd80716752e74879b5818f9a2c4c8b4e3585e
KantoAndrianina <kanto.andrianina@yahoo.com>
2024-06-16 19:59:37 +0300

    rectif

--- src/main/java/projet_s6/bibliotheque/repository/SanctionRepository.java
+++ src/main/java/projet_s6/bibliotheque/repository/SanctionRepository.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @Repository
 public interface SanctionRepository extends JpaRepository<Sanction, Integer> {
 
-   @Query("SELECT s FROM Sanction s WHERE s.idMembre = :idMembre AND s.dateFinSanction > :currentDate")
+    @Query("SELECT s FROM Sanction s WHERE s.idMembre = :idMembre AND s.dateFinSanction > :currentDate")
     Optional<Sanction> findByIdMembreAndDateFinSanctionAfter(@Param("idMembre") Integer idMembre, @Param("currentDate") Date currentDate);
 
     @Query("SELECT COUNT(s) > 0 FROM Sanction s WHERE s.idMembre = :idMembre AND s.dateFinSanction > :date")
